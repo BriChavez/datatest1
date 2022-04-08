f5ad55a1aad5c9bdce37b8f4a6cfe870ddce349c
BriChavez <brianachavez@gmail.com>
2022-04-08 15:11:17 -0700

    fine tunes the id style and adds more artists

--- test1.py
+++ test1.py
@@ -3,33 +3,45 @@
 # print(id_l7)
 # id_badu= random.sample(range(1,100),1)
 # print(id_badu)
-
 import random
-id_l7 = random.sample(range(1,100),1)
+# id_l7 = random.sample(range(1,100),1)
+# # id_l7 = id_l7 = rng
 
+
+l7= "band"
+id_l7=id(l7)
+id_l7 = f'{id_l7}'
+type(id_l7)
+
+# name_id="".join(str(id_l7))
 
 class Band:
-    def __init__(self, artist, artist_id, title, length, genre):
+    def __init__(self, artist, name_id, artist_id, title, length, genre):
         self.artist = artist
-        self.artist_id= artist_id
+        self.artist_id = artist_id
+        self.name_id = name_id
         self.title = title
         self.author = length
         self.genre = genre
+        
         print(artist)
         print(artist_id)
+        print(name_id)
         print(title)
         print(length)
         print(genre)
         
-l7 = Band("L7", id_l7, "Fast and Frightening", "2.24", "Riot Grrrls")
-l7.artist = l7_artist
-l7.artist_id
-print(l7_artist)
+l7 = Band("L7", "L7-" + id_l7, id_l7, "Fast and Frightening", "2.24", "Riot Grrrls")
+id_l7=id(l7)
+id_l7 = f'{id_l7}'
 
+badu = Band("Erykah Badu", "Erykah Badu-" + id_badu, id_badu, "Next Lifetime", "6.30", "R&B")
+id_badu=id(badu)
+id_badu = f'{id_badu}'
 
-Band("Erykah Badu", "Next Lifetime", "6.30", "R&B")
-Band("Manu Chao", "Bongo Bong", "4:13", "Reggae")
-
+manu = Band("Manu Chao", "Manu Chao-" + id_manu, id_manu, "Bongo Bong", "4:13", "Reggae")
+id_manu = id(manu)
+id_manu = f'{id_manu}'
 
 
 
