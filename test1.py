5731311fa65641522870e7f211c56c4163bb5fac
BriChavez <brianachavez@gmail.com>
2022-04-08 11:31:40 -0700

    adds s template and some artists

---
+++ test1.py
@@ -0,0 +1,18 @@
+
+
+# L7
+class Band:
+    def __init__(self, title, length, genre):
+        self.title = title
+        self.author = length
+        self.genre = genre
+    
+    def you_heard(self):
+        print(f"have you heard {self.title} by {self.author}?")
+        
+l7 = Band("L7", "Fast and Frightening", "2.24", "Riot Grrrls")
+
+
+
+
+
