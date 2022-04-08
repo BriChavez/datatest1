941090afb53574febd45c06f2b1a441ad581ad23
BriChavez <brianachavez@gmail.com>
2022-04-08 13:44:03 -0700

    defines the class variables and runs one band through and returns each

--- test1.py
+++ test1.py
@@ -1,16 +1,32 @@
+# import random
+# id_l7 = random.sample(range(1,100),1)
+# print(id_l7)
+# id_badu= random.sample(range(1,100),1)
+# print(id_badu)
+
+import random
+id_l7 = random.sample(range(1,100),1)
 
 
-# L7
 class Band:
-    def __init__(self, title, length, genre):
+    def __init__(self, artist, artist_id, title, length, genre):
+        self.artist = artist
+        self.artist_id= artist_id
         self.title = title
         self.author = length
         self.genre = genre
-    
-    def you_heard(self):
-        print(f"have you heard {self.title} by {self.author}?")
+        print(artist)
+        print(artist_id)
+        print(title)
+        print(length)
+        print(genre)
         
-Band("L7", "Fast and Frightening", "2.24", "Riot Grrrls")
+l7 = Band("L7", id_l7, "Fast and Frightening", "2.24", "Riot Grrrls")
+l7.artist = l7_artist
+l7.artist_id
+print(l7_artist)
+
+
 Band("Erykah Badu", "Next Lifetime", "6.30", "R&B")
 Band("Manu Chao", "Bongo Bong", "4:13", "Reggae")
 
