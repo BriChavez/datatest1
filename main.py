d1480247175bec0b66ed5853f321ef998c882fcd
BriChavez <brianachavez@gmail.com>
2022-04-08 23:45:35 -0700

    nailes it this time

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 class Artist:
     def __init__(self, artist, name_id, artist_id, dob, genre):
         self.artist = artist
@@ -26,10 +24,7 @@ class Song:
         print(length)
         print(lyrics)
 
-
-id_list = [id(1),id(2), id(3)]
-
-
+id_list = [id(1),id(2), id(3), id(4)]
 
 id_l7=f'{id_list[0]}'   
 l7 = Artist("L7", "L7-" + id_l7, id_l7, "August 1st, 1960", "Riot Grrrls")
@@ -43,12 +38,29 @@ id_manu=f'{id_list[2]}'
 manu = Artist("Manu Chao", "Manu Chao-" + id_manu, id_manu, "June 20th, 1961", "Reggae")
 manu_song = Song(id_manu, "Bongo Bong", "4:13", "Mama was queen of the mambo, Papa was king of the Congo, Deep down in a jungle, I started banging my first bongo, Every monkey like to be, In my place instead of me, 'Cause I'm the king of Bongo, baby, I'm the king of Bongo Bong")
 
-
 input_id = input()
 
 artist_id_dict = {"l7_key": id_l7, "badu_key": id_badu, "manu_key": id_manu}
 artist_list = list(artist_id_dict.values())
+
 if input_id in artist_list:
-    print("Let's pull up some information on your artist")
+    print("Your artist is listed")
 else: print("No Artist is listed with that ID")
 
+add_artist = input("Would you like to add an artist to this list?")
+
+if add_artist == "yes":
+    class New_Artist:
+        def __init__(self, inputed_name, inputed_song):
+            self.inputed_name = inputed_name
+            self.inputed_song = inputed_song
+            return self.inputed_name + '-' + self.inputed_song
+        @classmethod
+        def get_user_input(self):
+            inputed_name = input('Enter an artists name: ')
+            inputed_song = input('Enter a song name by this artist')
+            print(inputed_name, inputed_song)
+    new_artist = New_Artist.get_user_input()
+
+else:
+    print("That's okay, i hope you found what you're looking for")
